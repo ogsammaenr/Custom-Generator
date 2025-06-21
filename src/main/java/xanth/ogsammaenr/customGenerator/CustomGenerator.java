3007dc1ee72d9966f0655ce961e3296c7f0dcf82
ogsammaenr <osmangamer152@gmail.com>
2025-06-21 13:02:18 +0300

    initial commit

---
+++ src/main/java/xanth/ogsammaenr/customGenerator/CustomGenerator.java
@@ -0,0 +1,17 @@
+package xanth.ogsammaenr.customGenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
